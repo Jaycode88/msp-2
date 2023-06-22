4d47567660130a4695b3dc14f88b040e9e9c9b6a
Joe Seabrook <joeseabrook0306@gmail.com>
2023-06-22 12:43:32 +0000

    Add: startTimer, startGame and endGame functions, eventlistener for rat whack

--- assets/scripts/game-script.js
+++ assets/scripts/game-script.js
@@ -32,8 +32,42 @@ function randomBin(bins) {
     }, time);
   }
 
+  function startTimer(duration) {
+    let time = duration;
+    countdown = setInterval(() => {
+      time--;
+      timerElement.textContent = time.toString();
+      if (time <= 0) {
+        clearInterval(countdown);
+        endGame();
+      }
+    }, 1000); // sets the countdown to to update and display the time every second
+  }
 
+  function startGame() {
+    scoreBoard.textContent = '0';
+    timeUp = false;
+    score = 0;
+    timerElement.textContent = '60'; // Reset the timer value to 60
+    peep();
+    startTimer(60); // Start  countdown with 60 seconds
+  }
 
+  function endGame() {
+    timeUp = true;
+    clearTimeout(timer);
+    clearInterval(countdown);
+    // Additional logic to be added like score result shown etc
+  }
+
+  function whack(e) {
+    if (!e.isTrusted) return;
+    score++;
+    this.parentNode.classList.remove('up');
+    scoreBoard.textContent = score.toString();
+  }
+  
+  rats.forEach(rat => rat.addEventListener('click', whack));
 
 
 //module.exports = { randomTime };
