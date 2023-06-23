28d47b7ce3493815a1d68df16c8de844a3d9bb26
Joe Seabrook <joeseabrook0306@gmail.com>
2023-06-23 14:28:36 +0000

    Add: CSS styling, Beautify all code

--- assets/scripts/game-script.js
+++ assets/scripts/game-script.js
@@ -11,106 +11,106 @@ let timer;
 let countdown;
 
 function randomTime(min, max) {
-  return Math.round(Math.random() * (max - min) + min);
+    return Math.round(Math.random() * (max - min) + min);
 }
 
 function randomBin(bins) {
-  let index = Math.floor(Math.random() * bins.length);
-  let bin = bins[index];
-  if (bin === lastBin) {
-    return randomBin(bins);
-  }
-  lastBin = bin;
-  return bin;
+    let index = Math.floor(Math.random() * bins.length);
+    let bin = bins[index];
+    if (bin === lastBin) {
+        return randomBin(bins);
+    }
+    lastBin = bin;
+    return bin;
 }
 
 function rise() {
-  let ratBin = randomBin(bins);
-  let mouseBin = randomBin(bins);
-// Check if the same bin is selected for rat and mouse, and choose call rise() if necessary
-  if (ratBin === mouseBin) {
-    return rise();
-  }
+    let ratBin = randomBin(bins);
+    let mouseBin = randomBin(bins);
+    // Check if the same bin is selected for rat and mouse, and choose call rise() if necessary
+    if (ratBin === mouseBin) {
+        return rise();
+    }
 
-  let rat = ratBin.querySelector('.rat');
-  let mouse = mouseBin.querySelector('.mouse');
+    let rat = ratBin.querySelector('.rat');
+    let mouse = mouseBin.querySelector('.mouse');
 
-  // display rat from using animation by adding class 'up' 
-  ratBin.classList.add('up');
-  rat.style.display = 'block';
-//hide rat after random duration by removing class 'up'
-  setTimeout(() => {
-    ratBin.classList.remove('up');
-    rat.style.display = 'none';
-  }, randomTime(200, 2000));
-
-  setTimeout(() => { 
-    //After random time display mouse using animation  by adding class 'up
-    mouseBin.classList.add('up');
-    mouse.style.display = 'block';
+    // display rat from using animation by adding class 'up' 
+    ratBin.classList.add('up');
+    rat.style.display = 'block';
+    //hide rat after random duration by removing class 'up'
+    setTimeout(() => {
+        ratBin.classList.remove('up');
+        rat.style.display = 'none';
+    }, randomTime(200, 2000));
 
     setTimeout(() => {
-      //After rndom time remove mouse by removing class 'up'
-      mouseBin.classList.remove('up');
-      mouse.style.display = 'none';
+        //After random time display mouse using animation  by adding class 'up
+        mouseBin.classList.add('up');
+        mouse.style.display = 'block';
+
+        setTimeout(() => {
+            //After rndom time remove mouse by removing class 'up'
+            mouseBin.classList.remove('up');
+            mouse.style.display = 'none';
+        }, randomTime(200, 2000));
     }, randomTime(200, 2000));
-  }, randomTime(200, 2000));
-// if game not over , schedule for next rat or mouse
-  if (!timeUp) {
-    setTimeout(rise, randomTime(1000, 2000)); // Delay the next rat and mouse
-  }
+    // if game not over , schedule for next rat or mouse
+    if (!timeUp) {
+        setTimeout(rise, randomTime(1000, 2000)); // Delay the next rat and mouse
+    }
 }
 
 rats.forEach(rat => {
-  rat.style.display = 'none'; // Hide rats initially
+    rat.style.display = 'none'; // Hide rats initially
 });
 mice.forEach(mouse => {
-  mouse.style.display = 'none'; // Hide mice initially
+    mouse.style.display = 'none'; // Hide mice initially
 });
 
 
 
 function startTimer(duration) {
-  let time = duration;
-  countdown = setInterval(() => {
-    time--;
-    timerElement.textContent = time.toString();
-    if (time <= 0) {
-      clearInterval(countdown);
-      endGame();
-    }
-  }, 1000);
+    let time = duration;
+    countdown = setInterval(() => {
+        time--;
+        timerElement.textContent = time.toString();
+        if (time <= 0) {
+            clearInterval(countdown);
+            endGame();
+        }
+    }, 1000);
 }
 
 function startGame() {
-  gameArea.style.display = 'flex';
-  scoreBoard.textContent = '0';
-  timeUp = false;
-  score = 0;
-  timerElement.textContent = '60';
-  // Give 1 second delay for game area to appear before rise() and startTimer are called
-  setTimeout(() => {
-  rise();
-  startTimer(60);
-},1000);
+    gameArea.style.display = 'flex';
+    scoreBoard.textContent = '0';
+    timeUp = false;
+    score = 0;
+    timerElement.textContent = '60';
+    // Give 1 second delay for game area to appear before rise() and startTimer are called
+    setTimeout(() => {
+        rise();
+        startTimer(60);
+    }, 1000);
 }
 
 function endGame() {
-  timeUp = true;
-  clearTimeout(timer);
-  clearInterval(countdown);
-  // Additional logic to be added like score result shown, etc.
+    timeUp = true;
+    clearTimeout(timer);
+    clearInterval(countdown);
+    // Additional logic to be added like score result shown, etc.
 }
 
 function whack(e) {
-  if (!e.isTrusted) return;
-  if (this.classList.contains('rat')) {
-    score++;
-  } else if (this.classList.contains('mouse')) {
-    score--;
-  }
-  this.parentNode.classList.remove('up');
-  scoreBoard.textContent = score.toString();
+    if (!e.isTrusted) return;
+    if (this.classList.contains('rat')) {
+        score++;
+    } else if (this.classList.contains('mouse')) {
+        score--;
+    }
+    this.parentNode.classList.remove('up');
+    scoreBoard.textContent = score.toString();
 }
 
 rats.forEach(rat => rat.addEventListener('click', whack));
